d1ae83d775d457a25539199aaec4ecbb398f8dce
Scoffgard <teo.meilleurat@gmail.com>
2021-02-22 17:51:57 +0100

    Created the WebView system

--- resources/main/client/views/viewsLoader.mjs
+++ resources/main/client/views/viewsLoader.mjs
@@ -4,20 +4,30 @@ let views = {};
 
 alt.on('view:useMethod', function (id, method) {
     if (method == 'on') return console.log('This method isn\'t supported by this event.');
-    views[id].view[method](...Array.prototype.slice.call(arguments, 2));
+    const args = Array.prototype.slice.call(arguments, 2);
+    views[id].view[method](...args);
 });
 
-alt.on('view:load', (id) => {
+alt.on('view:accessProperties', function (id, actionType, properties, value) {
+    if (actionType == true) { //modify value
+        views[id].view[properties] = value;
+    } else {
+        alt.emit('value', views[id].view[properties]);
+    }
+});
+
+alt.on('view:load', (id, callbackName) => {
     let view = new alt.WebView(`http://resource/client/views/${id}/index.html`);
     
     view.on('loaded', () => {
         views[id] = {view, id};
+        if (callbackName) alt.emit(callbackName);
     });
     
 });
 
 alt.on('view:on', (id, eventName, callbackName) => {
-    views[id].view.on(eventName, data => {
-        alt.emit(callbackName, data);
+    views[id].view.on(eventName, function () {
+        alt.emit(callbackName, id, ...arguments);
     });
 });
