5e31870a3f2d3813bccf4e9c65a173effba4306e
Scoffgard <teo.meilleurat@gmail.com>
2021-02-23 01:33:06 +0100

    Moved the create char animations Start adding the capacity to create characters

--- resources/main/client/events/onClient.mjs
+++ resources/main/client/events/onClient.mjs
@@ -2,9 +2,34 @@ import * as alt from 'alt-client';
 import * as native from 'natives';
 let hidden = false;
 
+alt.on('anim:launchCharCreation', () => {
+    alt.toggleGameControls(false);
+
+    native.setEntityCoords(alt.Player.local.scriptID, 402.83, -996.53, -99.02, 0.0, 0.0, 0.0, true)
+    native.setEntityHeading(alt.Player.local.scriptID, 180.00);
+
+    const cam = native.createCam("DEFAULT_SCRIPTED_CAMERA", 1);
+    native.setCamCoord(cam, 402.81, -998.1, -98.5);
+	native.setCamRot(cam, 0, 0, 0);
+    native.setCamFov(cam, 50);
+    native.setCamActive(cam, true);
+    native.renderScriptCams(true, false, 0, true, false);
+});
+
+alt.on('anim:stopCharCreation', () => {
+    alt.toggleGameControls(true);
+
+    native.setEntityCoords(alt.Player.local.scriptID, -1037.82861328125, -2737.97802734375, 19.1640625, 0, 0, 0, true);
+
+    native.renderScriptCams(false, false, 0, true, false);
+});
+
 alt.on('callback:chooseChar', (id, char) => {
     alt.emit('view:accessProperties', id, true, 'isVisible', false);
     alt.emit('view:useMethod', id, 'unfocus');
+
+    if (char == 'new') return alt.emit('anim:launchCharCreation');
+
     alt.showCursor(false);
     alt.toggleGameControls(true);
     alt.Player.local.setMeta('characterID', char);
--- resources/main/client/events/onServer.mjs
+++ resources/main/client/events/onServer.mjs
@@ -1,27 +1,9 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
 
-alt.onServer('anim:launchCharCreation', () => {
-    alt.toggleGameControls(false);
+alt.onServer('anim:launchCharCreation', () => {alt.emit('anim:launchCharCreation')});
 
-    native.setEntityCoords(alt.Player.local.scriptID, 402.83, -996.53, -99.02, 0.0, 0.0, 0.0, true)
-    native.setEntityHeading(alt.Player.local.scriptID, 180.00);
-
-    const cam = native.createCam("DEFAULT_SCRIPTED_CAMERA", 1);
-    native.setCamCoord(cam, 402.81, -998.1, -98.5);
-	native.setCamRot(cam, 0, 0, 0);
-    native.setCamFov(cam, 50);
-    native.setCamActive(cam, true);
-    native.renderScriptCams(true, false, 0, true, false);
-});
-
-alt.onServer('anim:stopCharCreation', () => {
-    alt.toggleGameControls(true);
-
-    native.setEntityCoords(alt.Player.local.scriptID, -1037.82861328125, -2737.97802734375, 19.1640625, 0, 0, 0, true);
-
-    native.renderScriptCams(false, false, 0, true, false);
-});
+alt.onServer('anim:stopCharCreation', () => {alt.emit('anim:stopCharCreation')});
 
 alt.onServer('anim:playCutscene', cutscene => {
     native.requestCutscene(cutscene, 8);
